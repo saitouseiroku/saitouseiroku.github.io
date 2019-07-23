cdeb59a8ad6b0dd8af36cc86e08c970b6e38023f
YujiIkuta <y.ikuta@comradesol.com>
2019-07-23 11:30:14 +0900

    Update page

--- test/test.js
+++ test/test.js
@@ -9,6 +9,21 @@ var PostNo = function(){
 
 	console.log(textByLine);
 
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", "./test.csv", false);
+	rawFile.onreadystatechange = function ()
+	{
+		if(rawFile.readyState === 4)
+		{
+			if(rawFile.status === 200 || rawFile.status == 0)
+			{
+				var allText = rawFile.responseText;
+				alert(allText); 
+			}
+		}
+	}
+	rawFile.send(null);
+
 	// ”ñ“¯Šú
 	/*
 	fs.readFile("./test.txt", function(text){
