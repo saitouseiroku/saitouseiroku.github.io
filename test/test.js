49042ed720d732ddf66097b70604874aad872b9d
YujiIkuta <y.ikuta@comradesol.com>
2019-07-23 11:11:53 +0900

    Update page

--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
 var PostNo = function(){
+
 	console.log("index");
+
+	var fs = require("fs");
+	// “¯Šú
+	var text = fs.readFileSync("./test.csv");
+	var textByLine = text.split("\n");
+
+	console.log(textByLine);
+
+	// ”ñ“¯Šú
+	/*
+	fs.readFile("./test.txt", function(text){
+		var textByLine = text.split("\n")
+	});
+	*/
 }
